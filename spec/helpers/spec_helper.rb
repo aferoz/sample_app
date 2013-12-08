77842f4dc7b021773e4b368f7586ee0311da8180
Azeem Feroz <aferoz@gmail.com>
2013-12-08 12:30:35 -0600

    Make a basic User model (inclduing secure passwords)

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -15,7 +15,6 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.include ApplicationHelper
-  
 
 
   # ## Mock Framework
